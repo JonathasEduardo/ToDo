d7dce5ef1c34955880ce95110b7e085cc1680dcd
Jonathas Eduardo <jonathaseduardof@gmail.com>
2024-01-05 14:44:18 -0300

    Tirar o usuario root e colocar um DBadmin

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,18 +1,12 @@
-// Importa o módulo 'knex'
+const envConfig = require('../database/envConfig');
+
 let knex = require('knex')({
-  // Especifica o cliente de banco de dados como MySQL
   client: 'mysql2',
-  // Configuração da conexão com o banco de dados
   connection: {
-      // Host do banco de dados (por padrão localhost)
-      host : 'localhost',
-      // Nome de usuário para autenticação (por padrão root)
-      user : 'root',
-      // Senha para autenticação (substitua pela sua senha definida)
-      password : 'Jonathas2001',
-      // Nome do banco de dados criado (substitua pelo nome do seu banco)
-      database : 'todo'
-  }
-});
-// Exporta a configuração do knex para uso em outros módulos
-module.exports = knex;
+    host: envConfig.host,
+    user: envConfig.user,
+    password: envConfig.password,
+    database: envConfig.database,   
+   }
+}); 
+module.exports = knex
---
+++ src/database/envConfig.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+
+module.exports = {
+  user: 'DBadmin',
+  password: 'hllEHB6Z2LGw0o',
+  database: 'todo',
+  host: 'localhost',
+};
